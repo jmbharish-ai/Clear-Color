ce84ffab2b189e34614691de38813f1538151680
jmbharish-ai <jmbharish@gmail.com>
2026-07-27 21:50:01 -0400

    Update game.js

--- game.js
+++ game.js
@@ -40,7 +40,9 @@ var subPerfectColors=[
     color("1", "B", "1", "G", "3/4"),
     color("1", "B", "1", "G", "1/2"),
     color("1/2", "B", "1/4", "R", "1/2"),
-    color("1/2", "B", "1", "G", "3/4")
+    color("1/2", "B", "1", "G", "3/4"),
+    
+    "rgb(191, 255, 128)"
 ];
 var currentSubPerfectColors=[]; for(var i=0; i<subPerfectColors.length; i++) currentSubPerfectColors[i]=subPerfectColors[i];
 function randomCurrentSubPerfectColor(){
